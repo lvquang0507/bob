5fd943608ef606ad63fef37d54c02719b98e6715
lvquang0507 <levinhquang757@gmail.com>
2023-02-25 21:41:14 +0700

    add ros2_control

--- launch/rsp_gz.launch.py
+++ launch/rsp_gz.launch.py
@@ -3,9 +3,15 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 
 # from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+    ExecuteProcess,
+)
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -41,4 +47,51 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription([rsp, gazebo, spawn_entity])
+    joint_state_broadcaster_spawn = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "joint_state_broadcaster",
+        ],
+        output="screen",
+    )
+
+    diff_drive_spawner = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "diff_drive_base_controller",
+        ],
+        output="screen",
+    )
+
+    # joint_broadcaster_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["joint_state_broadcaster"],
+    # )
+
+    return LaunchDescription(
+        [
+            rsp,
+            gazebo,
+            spawn_entity,
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_entity, on_exit=[joint_state_broadcaster_spawn]
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawn,
+                    on_exit=[diff_drive_spawner],
+                )
+            ),
+        ]
+    )
